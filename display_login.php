4fd87b8b825739dbe89dd429bc8521754dc7a13d
oliviawelsch <ow9@njit.edu>
2018-10-17 11:40:50 -0400

    first try error message

--- display_login.php
+++ display_login.php
@@ -1,6 +1,25 @@
 <html>
 <body>
 
+<?php
+$emailErr = $passwordErr = "";
+$email = $password = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (empty($_POST["email_address"])) {
+$emailErr = "Email address is required";
+} else {
+$email = test_input($_POST["email_address"]);
+}
+
+if (empty($_POST["password"])) {
+$passwordErr = "Password is required";
+} else {
+$email = test_input($_POST["password"]);
+}
+
+?>
+
 Your email address is: <?php echo $_POST["email_address"]; ?><br>
 Your password is: <?php echo $_POST["password"]; ?>
 
