5a46bc1d3ebc83d53471aad015c81efcb53602b4
oliviawelsch <ow9@njit.edu>
2018-10-17 10:33:57 -0400

    PHP Registration Draft

--- display_login.php
+++ display_login.php
@@ -2,7 +2,7 @@
 <body>
 
 Your email address is: <?php echo $_POST["email_address"]; ?><br>
-Your passowrd is: <?php echo $_POST["password"]; ?>
+Your password is: <?php echo $_POST["password"]; ?>
 
 </body>
 </html>
---
+++ display_registration.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Your first name is: <?php echo $_POST["first_name"]; ?><br>
+Your last name is: <?php echo $_POST["last_name"]; ?><br>
+Your birthday is: <?php echo $_POST["birthday"]; ?><br>
+Your email address is: <?php echo $_POST["email_address"]; ?><br>
+Your password is: <?php echo $_POST["password"]; ?>
+
+</body>
+</html>
