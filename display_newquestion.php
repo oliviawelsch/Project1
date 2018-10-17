b6f7a2290510eddf840e95be26a4f1996814ff89
oliviawelsch <ow9@njit.edu>
2018-10-17 10:38:22 -0400

    PHP Question Draft

---
+++ display_newquestion.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Your question name is: <?php echo $_POST["question_name"]; ?><br>
+Your question body is: <?php echo $_POST["question_body"]; ?><br>
+Your question skills are: <?php echo $_POST["question_skills"]; ?>
+
+</body>
+</html>
