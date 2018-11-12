b59d35d4888a08c96147a4982e4aa12aa7baa700
oliviawelsch <ow9@njit.edu>
2018-11-11 20:56:15 -0500

    form css complete

--- login.php
+++ login.php
@@ -3,34 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <title>Login Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
 
+<div class="container">
 <main>
-    <h1>Login Form</h1>
+    <h2>Login Form</h2>
 
-<form action="display_login.php" method="post">
+<form class="form-horizontal" action="display_login.php" method="post">
+    <div class="form-group">
+        <label class="control-label col-sm-2" for="email">Email Address:</label>
+    <div class="col-sm-10">
+        <input type="text" class="form-control" id="email" placeholder="Enter Email Address" name="email_address">
+    </div>
+    </div>
 
-    <table>
-        <tr>
-    <div id="data">
-        <td>Email Address:</td>
-        <td><input type="text" name="email_address"><td>
-    </tr>
+<div class="form-group">
+    <label class="control-label col-sm-2" for="'pwd">Password:</label>
+    <div class="col-sm-10">
+            <input type="text" class="form-control" id="pwd" placeholder="Enter Password" name="password">
+    </div>
+</div>
 
-        <tr>
-            <td>Password:</td>
-            <td><input type="text" name="password"></td>
-
-        </tr>
-        </div>
-    </table>
 
     <div id="buttons">
+        <label>&nbsp;</label>
         <input type="submit" value="Log In"><br>
     </div>
+
 </form>
+</div>
 
 </main>
 </body>
--- registration.php
+++ registration.php
@@ -3,31 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <title>Registration Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="registration.css">
 </head>
 <body>
-
+<div class="container">
 
 <main>
-    <h1>Registration Form</h1>
+    <h2>Registration Form</h2>
 
-    <form action="display_registration.php" method="post">
+    <form class="form-horizontal" action="display_registration.php" method="post">
 
-        <div id="data">
-            <label>First Name:</label>
-            <input type="text" name="first_name"><br>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="first_name">First Name:</label>
+            <div class="col-sm-10">
+                <input type="text" name="first_name" class="form-control" id="first_name" placeholder="Enter Your First Name">
+            </div>
+        </div>
 
-            <label>Last Name</label>
-            <input type="text" name="last_name"><br>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="last_name">Last Name:</label>
+            <div class="col-sm-10">
+                <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Enter Your Last Name">
+            </div>
+        </div>
 
-            <label>Birthday</label>
-            <input type="date" name="birthday"><br>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="birthday">Birthday:</label>
+            <div class="col-sm-10">
+                <input type="date" name="birthday" class="form-control" id="birthday" placeholder="Enter Your Birthday">
+            </div>
+        </div>
 
-            <label>Email Address:</label>
-            <input type="text" name="email_address"><br>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email_address">Email Address:</label>
+            <div class="col-sm-10">
+                <input type="text" name="email_address" class="form-control" id="email_address" placeholder="Enter Your Email Address">
+            </div>
+        </div>
 
-            <label>Password:</label>
-            <input type="password" name="password"><br>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="password">Password:</label>
+            <div class="col-sm-10">
+                <input type="text" name="password" class="form-control" id="password" placeholder="Enter Your Password">
+            </div>
         </div>
 
         <div id="buttons">
@@ -37,5 +58,6 @@
     </form>
 
 </main>
+</div>
 </body>
 </html>
